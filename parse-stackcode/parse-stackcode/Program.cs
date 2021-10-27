372241cef28036361b71b17d8dfc0e553d62fade
Roman Pavlov <rspaulau@gmail.com>
2021-10-26 17:25:54 -0700

    initial migration to .net 5

--- parse-stackcode/parse-stackcode/Program.cs
+++ parse-stackcode/parse-stackcode/Program.cs
@@ -1,84 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Fizzler.Systems.HtmlAgilityPack;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace parse_stackcode
 {
     class Program
     {
-        private const string Pretitle = "A daily screenshot from the Stack Overflow codebase";
-        private static readonly Regex TitleRegex = new Regex(@"((A daily screenshot from the Stack Overflow codebase \((.+)\).+)#StackCode)");
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            using var tagsFile = File.OpenRead("tags.json");
+            var tagLookup = await JsonSerializer.DeserializeAsync<Dictionary<string, List<string>>>(tagsFile);
+
+            List<StackCode> stackCodes = ParseStackCodes("stackcode-dump.html", tagLookup);
+
+            if (stackCodes.Any())
+            {
+                var directory = Directory.CreateDirectory("../../../../../_posts");
+                WriteStackCodes(stackCodes, directory);
+            }
+        }
+
+        private static List<StackCode> ParseStackCodes(string path, Dictionary<string, List<string>> tagLookup)
+        {
+            var preTitle = "A daily screenshot from the Stack Overflow codebase";
+            var titleRegex = new Regex(@"((A daily screenshot from the Stack Overflow codebase \((.+)\).+)#StackCode)");
             var stackCodes = new List<StackCode>();
-            using (var file = File.OpenRead("stackcode-dump.html"))
+            using (var file = File.OpenRead(path))
             {
                 var document = new HtmlDocument();
                 document.Load(file);
                 var page = document.DocumentNode;
-                var headerNodes = page.QuerySelectorAll(".stream-item-header");
-                foreach (var headerNode in headerNodes)
+                var tweets = page.QuerySelectorAll("li.stream-item");
+                foreach (var tweet in tweets)
                 {
-                    var timeNode = headerNode.QuerySelector(".time");
-                    var textNode = headerNode.NextSibling;
-                    var tweetNode = textNode.NextSibling;
+                    var timeNode = tweet.QuerySelector(".time");
+                    var tweetTextNode = tweet.QuerySelector(".tweet-text");
+                    var tweetId = tweet.Attributes["data-item-id"].Value;
 
-                    var tweet = tweetNode.InnerText;
-                    if (tweet.Contains(Pretitle))
+                    var tweetText = tweetTextNode.InnerText;
+                    if (tweetText.Contains(preTitle))
                     {
                         var timestampNode = timeNode.QuerySelector("._timestamp");
-                        var tweetTimestampNode = timeNode.QuerySelector(".tweet-timestamp");
                         var timestamp = timestampNode.Attributes["data-time"].Value;
 
-                        var titleMatch = TitleRegex.Match(tweet);
-                        var text = titleMatch.Groups[2].Value;
-                        var title = FirstLetterToUpper(titleMatch.Groups[3].Value);
+                        var titleMatch = titleRegex.Match(tweetText);
+                        var text = WebUtility.HtmlDecode(titleMatch.Groups[2].Value);
+                        var title = WebUtility.HtmlDecode(titleMatch.Groups[3].Value);
+                        title = FirstLetterToUpper(title);
 
-
-                        var thumbnailNode = tweetNode.QuerySelector(".twitter-timeline-link");
+                        var thumbnailNode = tweetTextNode.QuerySelector(".twitter-timeline-link");
                         var thumbnailhandle = thumbnailNode.InnerText.Split('/')[1];
 
                         stackCodes.Add(new StackCode
                         {
                             Layout = "stackcode",
                             Name = "Nick Craver",
-                            Twitterhandle = "Nick_Craver",
+                            TwitterHandle = "Nick_Craver",
                             Title = title,
                             Text = text,
-                            Thumbnailhandle = thumbnailhandle,
-                            Tweet = $"https://twitter.com{tweetTimestampNode.Attributes["href"].Value}",
+                            ThumbnailHandle = thumbnailhandle,
+                            Tweet = $"https://twitter.com/Nick_Craver/status/{tweetId}",
                             Date = UnixTimeStampToDateTime(double.Parse(timestamp)),
-                            Tags = new[] { "not-mapped" }
+                            Tags = BuildTags(tweetId, tweetText, tagLookup)
                         });
                     }
                 }
             }
+
+            return stackCodes;
+        }
+
+        private static IEnumerable<string> BuildTags(string tweetId, string tweetText, Dictionary<string, List<string>> tagLookup)
+        {
+            return tagLookup.TryGetValue(tweetId, out var tags) ? tags : new[] { "not-mapped" };
+        }
+
+        private static void WriteStackCodes(List<StackCode> stackCodes, DirectoryInfo outputDirectory)
+        {
             foreach (var stackCode in stackCodes)
             {
-                var directory = Directory.CreateDirectory("_posts");
-
                 var safeForFilenameTitle = GetSafeFilename(NormalizeTitle(stackCode));
 
-                var fileName = Path.Combine(directory.Name, $"{stackCode.Date.ToString("yyyy-MM-dd")}-{safeForFilenameTitle}.md");
-                File.WriteAllText(fileName, $@"---
+                var fileName = Path.Combine(outputDirectory.FullName, $"{stackCode.Date:yyyy-MM-dd}-{safeForFilenameTitle}.md");
+                var contents = $@"---
 layout: {stackCode.Layout}
 name: {stackCode.Name}
-twitterhandle: {stackCode.Twitterhandle}
+twitterhandle: {stackCode.TwitterHandle}
 title: ""{stackCode.Title}.""
 text: ""{stackCode.Text}""
-thumbnailhandle: {stackCode.Thumbnailhandle}
+thumbnailhandle: {stackCode.ThumbnailHandle}
 tweet: {stackCode.Tweet}
 date: {stackCode.Date:yyyy-MM-dd HH:mm:ss+0000}
-tags: [""{stackCode.Tags[0]}""]
+tags: {JsonSerializer.Serialize(stackCode.Tags)}
 ---
-");
+";
+                File.WriteAllText(fileName, contents);
             }
         }
 
@@ -87,21 +110,19 @@ tags: [""{stackCode.Tags[0]}""]
             var lowercase = stackCode.Title.ToLowerInvariant();
             return lowercase
                 .Replace(" ", "-")
-                .Replace(",", "");
+                .Replace(",", string.Empty);
         }
 
         public static string GetSafeFilename(string filename)
         {
-
-            return string.Join("", filename.Split(Path.GetInvalidFileNameChars()));
-
+            return string.Join(string.Empty, filename.Split(Path.GetInvalidFileNameChars()));
         }
 
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
             // Unix timestamp is seconds past epoch
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToUniversalTime();
             return dtDateTime;
         }
 
@@ -121,12 +142,12 @@ tags: [""{stackCode.Tags[0]}""]
     {
         public string Layout { get; set; }
         public string Name { get; set; }
-        public string Twitterhandle { get; set; }
+        public string TwitterHandle { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public string Thumbnailhandle { get; set; }
+        public string ThumbnailHandle { get; set; }
         public string Tweet { get; set; }
         public DateTime Date { get; set; }
-        public string[] Tags { get; set; }
+        public IEnumerable<string> Tags { get; set; }
     }
 }
